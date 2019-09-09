a8ec726bf35909e8ab6b13d1d69fcfe6a38e6c6e
Okyfunoky <okyfunoky@hotmail.com>
2019-09-09 13:04:57 -0700

    Adding functioning url builder

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -2,10 +2,38 @@ var key = "JoBtQcUS2jkjGtZFDpxjxNArrGOKzURf"
 var searchTerm = "election"
 
 
-function constructUrl(searchTerm, resultCount, yearStar, yearEnd){
+function constructUrl(searchTerm, resultCount, yearStart, yearEnd){
     var baseUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
     var keyUrl = "&api-key=" + key;
 
+    
+    console.log("year start:" + yearStart);
+    console.log("year end:" + yearEnd);
+    
+    //https://api.nytimes.com/svc/search/v2/articlesearch.json?
+    //q=election
+    //&begin_date=20120101
+    //&end_date=20121231
+    //&api-key=JoBtQcUS2jkjGtZFDpxjxNArrGOKzURf
+    //build url sequentially
     baseUrl += searchTerm;
+
+    //check if yearstart was set
+    if(yearStart){
+        //append the begin date clause to the url
+        console.log("Getting here");
+        baseUrl += "&begin_date=" + yearStart
+    }
+
+    //check if year end was set
+    if(yearEnd){
+        //append the end date clause to the url
+        baseUrl += "&end_date=" + yearEnd
+    }
+    //add key on last
     baseUrl += keyUrl;
+    return baseUrl;
 }
+
+// var test = constructUrl("boris+johnson",5,"20120101","20121231");
+// console.log(test);
