bc674afc3afee6dcc5cbff6fafc575388b6b8d2f
Okyfunoky <okyfunoky@hotmail.com>
2019-09-09 12:25:55 -0700

    Basic url builder

---
+++ assets/scripts/logic.js
@@ -0,0 +1,6 @@
+var key = "JoBtQcUS2jkjGtZFDpxjxNArrGOKzURf"
+var searchTerm = "election"
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + key
+
+console.log(url);
